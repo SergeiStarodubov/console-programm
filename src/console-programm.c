3998b8bbe07801bf1e6a03a2196943ca36e015b2
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-07-19 22:48:16 +0400

    set avarage

--- src/console-programm.c
+++ src/console-programm.c
@@ -11,7 +11,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+int main(void)
+{
+	int a, b, c;
+	printf("input three different integers: ");
+	scanf("%d%d%d",&a, &b, &c );
+	printf("sum is %d\n", a + b + c);
+	printf("average is %d\n", (a + b + c) /3);
+	printf("product is %d\n", a*b*c);
+
+	int smallest, biggest;
+
+	if (a > b && a > c) biggest = a;
+	if (b > a && b > c) biggest = b;
+	if (c > a && c > b) biggest = c;
+
+	if (a < b && a < c) smallest = a;
+	if (b < a && b < c) smallest = b;
+	if (c < a && c < b) smallest = c;
+
+	printf("smallest is %d\n", smallest);
+	printf("biggest is %d", biggest);
+
+	return 0;
 }
