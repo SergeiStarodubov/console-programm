d97afe6cca610471c39715b887106a2f7507f6cc
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-07-30 23:42:40 +0400

    Add bubblesorting

--- src/main.c
+++ src/main.c
@@ -3,17 +3,28 @@
 #include <math.h>
 #include <stdbool.h> 
 
-void bubbleSorting(int []);
+void bubbleSorting(int [], int);
 void toStringArray(int [], int);
 int main() {    
 	int arr[10] = {1,5,4,9,10,21,2,42,8,6};
+	bubbleSorting(arr, 10);
 	toStringArray(arr, 10);
     return 0;
 }
 
-void bubbleSorting(int array [])
+void bubbleSorting(int array [], int size)
 {
-	
+	int semaphor = 1;
+	for (int i = 0; i < size-1; i++)
+	{
+		if (array[i] > array[i+1]) {
+			int tmp = array[i];
+			array[i] = array[i+1];
+			array[i+1] = tmp;
+			semaphor = 0;
+		}
+	}
+	if (semaphor == 0) bubbleSorting(array, size);
 }
 
 void toStringArray(int array[], int size)
@@ -22,7 +33,7 @@ void toStringArray(int array[], int size)
 	for (int i = 0; i < size; i++)
 	{
 		if (i == 0)	printf("[%d,", array[i]);
-		else if  (i == size -1)printf("%d]", array[i]);
+		else if  (i == size -1)printf("%d]\n", array[i]);
 		else printf("%d,", array[i]);
 	}
 	
