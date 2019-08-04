66b967323a5b592e75fdc960c9d3d4c99db9d94b
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-08-04 19:38:56 +0400

    shuffle and deal cards

--- src/cards.c
+++ src/cards.c
@@ -3,9 +3,68 @@
 #include <math.h>
 #include <stdbool.h> 
 #include <ctype.h>
+#include <time.h>
+
+void shuffle(int wDeck[][13]);
+void deal(const int wDeck [] [13], const char* wFace[], const char* wSuit[]);
 
 int main ()
 {
-    puts("hello");
+    const char* suit[4] = {"hearts", "diamonds", "clubs", "spades"};
+    const char* face[13] = {
+		"Ace", "Deuce","Three",
+		"Four","Five", "Six",
+		"Seven","Eight","Nine", 
+		"Ten","Jack","Queen",
+		"King"};
+    
+    int deck [4][13] = {0};
+    srand(time(0));
+    shuffle(deck);
+    deal(deck,face,suit);
+
     return 0;
+}
+
+void deal (const int wDeck[][13], const char* wFace[], const char* wSuit[])
+{
+    int card;
+    int row;
+    int column;
+
+    for (card = 1; card <= 52; card++)
+    {
+        for (row  = 0; row <= 3; row++)
+        {
+            for (column = 0; column <= 12 ; column++)
+            {
+                if (wDeck[row][column] == card) 
+                {
+                    printf("%s of %-8s\n", wFace[column], wSuit[row]);
+                }
+                
+            }
+            
+        }
+        
+    }
+    
+}
+
+void shuffle(int wDeck[][13])
+{
+    int row;
+    int column;
+    int card;
+
+    for (card = 1; card <= 52; card++)
+    {
+        do
+        {
+            row = rand() % 4;
+            column = rand() % 13;
+        } while (wDeck[row][column] != 0);
+        wDeck[row][column] = card;
+    }
+    
 }
