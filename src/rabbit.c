0342ada811aa4f42165f8e9995496a95426bed76
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-08-07 21:43:59 +0400

    before render

--- src/rabbit.c
+++ src/rabbit.c
@@ -5,21 +5,65 @@
 #include <ctype.h>
 #include <time.h>
 #include <unistd.h>
+#define ROAD_LENGTH 80
 
-int* moveTurtle(void);
-int* moveRabbit(void);
-void toStringArray(int array[], int size);
+int* getMoveTurtle(void);
+int* getMoveRabbit(void);
+void sayStart(void);
+void toStringArray(char array[], int size);
+int checkWinner(int*, int*);
+void renderCompititerOnRoad(int*, int*, const int[ROAD_LENGTH]);
 
 int main()
 {   
     srand(time(0));
-    int road[80] = {0};
-    for (int i = 0; i < 80; i++)
-        road[i] = i;
+    int road[ ROAD_LENGTH ] = {0};
+    for (int i = 0; i < ROAD_LENGTH; i++) road[i] = i;
+    int* p_rabbit = &road[0];
+    int* p_turtle = &road[75];
+    sayStart();
+    while (true)
+    {   
+        if (checkWinner(p_rabbit, p_turtle) != 0) return;
+        sleep(1);
+    }   
     return 0;
 }
 
-int* moveTurtle(void)
+void renderCompititerOnRoad(int* p_rabbit, int* p_turtle ,const int road[ROAD_LENGTH])
+{
+    
+}
+
+/*  if function return 1 the winner is rabbit 
+    if function return -1 the winner turtle    
+    if function return 0 the competition keeps going still */ 
+int checkWinner(int* p_rabbit, int* p_turtle)
+{
+    if (*p_rabbit >= 70 && *p_turtle >= 70) {
+        puts("TURTLE WINS");
+        return -1;
+    }
+    if(*p_rabbit >= 70) {
+        puts("RABBIT WINS");
+        return 1;
+    }
+    if(*p_turtle >= 70) {
+        puts("TURTLE WINS");
+        return -1;
+    }
+    return 0;
+}
+
+
+void sayStart(void)
+{
+    puts("BANG");
+    puts("AND THE'RE OFF");
+}
+
+
+int* getMoveTurtle(void)
 {   
     static int move[2] = {0};
     int randNum;
@@ -37,7 +81,7 @@ int* moveTurtle(void)
     return &move[0];
 }   
 
-int* moveRabbit(void)
+int* getMoveRabbit(void)
 {   
     static int motion[2] = {0};
     int randNum;
@@ -61,14 +105,14 @@ int* moveRabbit(void)
     return &motion[0];
 }   
 
-void toStringArray(int array[], int size)
+void toStringArray(char array[], int size)
 {	
 
 	for (int i = 0; i < size; i++)
 	{
-		if (i == 0)	printf("[%d,", array[i]);
-		else if  (i == size -1)printf("%d]\n", array[i]);
-		else printf("%d,", array[i]);
+		if (i == 0)	printf("[%c,", array[i]);
+		else if  (i == size -1)printf("%c]\n", array[i]);
+		else printf("%c,", array[i]);
 	}
 	
 }
