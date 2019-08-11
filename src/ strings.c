968775c021229dd6142b68af75832ef06049415f
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-08-11 19:41:11 +0400

    new topic

---
+++ src/ strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h> 
+#include <ctype.h>
+#include <time.h>
+
+int main(void)
+{
+    char color[] = "blue";
+    char* colors = "red";
+    return 0;
+}
