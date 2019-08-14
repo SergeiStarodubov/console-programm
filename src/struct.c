93373139e65cf52ffd799a45f7873314acadf4df
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-08-14 20:29:49 +0400

    add struct

---
+++ src/struct.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h> 
+#include <ctype.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(void)
+{
+    
+    return 0;
+}
