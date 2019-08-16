c24242c0bab364f63964625175ab386983ff04dd
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-08-16 23:06:22 +0400

    add files

---
+++ src/files.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h> 
+#include <ctype.h>
+#include <time.h>
+#include <unistd.h>
+
+int main() {
+    int account;
+    char name[30];
+    double balance;
+
+    FILE* ptr;
+
+    if ((ptr = fopen("clients.dat", "w") == NULL)) puts("the file is not found");
+    else {
+        puts
+    }
+    return 0;
+}
