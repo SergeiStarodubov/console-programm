8de35605938f4bb132463a6c543fbe7d9c993ef9
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-08-16 23:22:04 +0400

    fix files

--- src/files.c
+++ src/files.c
@@ -9,13 +9,17 @@
 int main() {
     int account;
     char name[30];
-    double balance;
+    float balance;
 
     FILE* ptr;
 
-    if ((ptr = fopen("clients.dat", "w") == NULL)) puts("the file is not found");
+    if ((ptr = fopen("clients.dat", "w")) == NULL) puts("the file is not found");
     else {
-        puts
+        puts("--------------");
+        scanf("%d%s%1f", &account, name, &balance);
+        while (!feof(stdin))
+            fprintf(ptr, "%d %s %.2f\n", account, name, balance);
+        fclose(ptr);
     }
     return 0;
 }
