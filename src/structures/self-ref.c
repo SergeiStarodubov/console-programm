54029a089b9cd0bafd8e9fe2740db0b102ba58d5
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-08-19 23:35:34 +0400

    simple linked list

--- src/structures/self-ref.c
+++ src/structures/self-ref.c
@@ -7,14 +7,18 @@
 #include <unistd.h>
 
 
-typedef struct 
+typedef struct listNode
 {
     int data;
-    struct Node* pointer;
-} Node ;
+    struct listNode* next;
+} ListNode;
 
 int main(void)
 {
-
+    ListNode node1 = {10 , NULL};
+    ListNode node2 = {20 , (ListNode*) &node1};
+    ListNode node3 = {3 , (ListNode*) &node2};
+    ListNode node4 = {4 , (ListNode*) &node3};
+    printf("value of node is %d\n",node4.next->next->data);
     return 0;
 }
