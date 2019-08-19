b19cedab36fdd470404551a94310dc5c5996aa35
SergeiStarodubov <s.s.starodubov@yandex.ru>
2019-08-19 22:21:35 +0400

    struct

---
+++ src/structures/self-ref.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h> 
+#include <ctype.h>
+#include <time.h>
+#include <unistd.h>
+
+
+typedef struct 
+{
+    int data;
+    struct Node* pointer;
+} Node ;
+
+int main(void)
+{
+
+    return 0;
+}
