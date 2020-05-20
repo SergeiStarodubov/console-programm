d2f02a80c344e2fb46c09519d425502f1aa7c2c6
starodubov <sergei.starodubov@simbirsoft.com>
2020-05-20 11:04:19 +0400

    simple shell

--- shell/main.c
+++ shell/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
 
 int main() {
-    printf("Hello, World!\n");
+
+    char command[BUFSIZ];
+    int status;
+    pid_t pid;
+
+    for (;;) {
+        printf("shell: ");
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            printf("\n");
+            return 0;
+        }
+        command[strlen(command) - 1] = '\0';
+        if ((pid = fork()) == 0)
+            execlp(command, command, 0);
+        while (wait(&status) != pid)
+            continue;
+        printf("\n");
+    }
     return 0;
 }
+
+
